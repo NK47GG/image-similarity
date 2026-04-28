c2e9f7f36c6f3715b1653d97fc2e197da351d040
Nikzz. <64414504+NK47GG@users.noreply.github.com>
2026-04-28 20:55:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -206,7 +206,8 @@ with right_col:
                     <p style='color:rgba(140,120,180,0.5); font-size:0.7rem;'>ID: {pid}</p>
                 """, unsafe_allow_html=True)
                 st.markdown('</div>', unsafe_allow_html=True)
-                st.write(list(image_lookup.items())[:3])  # lihat sample key-value
-                st.write(type(db_df['id'].iloc[0]))       # lihat tipe id di database
+                
+                #st.write(list(image_lookup.items())[:3])  # lihat sample key-value
+                #st.write(type(db_df['id'].iloc[0]))       # lihat tipe id di database
     else:
         st.info("Silakan upload gambar untuk melihat hasil.")
